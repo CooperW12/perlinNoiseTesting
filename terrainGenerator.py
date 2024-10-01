34cd3bb7c5db70df1646956c0043cf5bc32f620f
Cooper Wright <73917610+CooperW12@users.noreply.github.com>
2024-10-01 15:25:53 -0500

    added trees in the forests and rare wreckage to the edge of beaches

--- terrainGenerator.py
+++ terrainGenerator.py
@@ -7,7 +7,7 @@ import random
 
 shape= (1024, 1024)
 scale = 100
-octaves = 2
+octaves = 4
 persistence =.5
 lacunarity = 2
 
@@ -27,9 +27,13 @@ for i in range(shape[0]):
         
 deepOcean = [94, 129, 172]
 ocean = [115, 146, 183]
+fish = [58, 75, 94]
 beach = [235, 203, 139]
+seashell = [185, 85, 189]
+wreckage = [105, 70, 1]
 land = [143, 176, 115]
-forest = [163, 190, 140]
+tree = [15, 64, 0]
+forest = [106, 122, 92]
 mountain = [76, 86, 106]
 tallMountain = [121, 133, 159]
 snow = [216, 222, 233]
@@ -44,15 +48,26 @@ def addColor(world):
                 coloredWorld[i][j] = deepOcean
             elif world[i][j] < 0:
                 coloredWorld[i][j] = ocean
-            elif world[i][j] < .1:
-                coloredWorld[i][j] = beach
-            elif world[i][j] < .2:
-                coloredWorld[i][j] = land
+            elif world[i][j] < .05:
+                if world[i][j] < .01:
+                    r = random.randint(0,100)
+                    if r < 1:
+                        coloredWorld[i][j] = wreckage
+                    else:
+                        coloredWorld[i][j] = beach
+                else:
+                    coloredWorld[i][j] = beach
+            elif world[i][j] < .15:
+                    coloredWorld[i][j] = land
+            elif world[i][j] < .25:
+                r = random.randint(0,100)
+                if r < 10:
+                   coloredWorld[i][j] = tree
+                else:
+                    coloredWorld[i][j] = forest
             elif world[i][j] < .3:
-                coloredWorld[i][j] = forest
-            elif world[i][j] < .4:
                 coloredWorld[i][j] = mountain
-            elif world[i][j] < .5:
+            elif world[i][j] < .4:
                 coloredWorld[i][j] = tallMountain
             elif world[i][j] < 1:
                 coloredWorld[i][j] = snow
